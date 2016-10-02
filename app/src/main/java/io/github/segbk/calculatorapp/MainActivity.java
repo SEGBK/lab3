375f04c09cf34aaa15f2ff1c20fad5fadd0bea41
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2016-10-02 13:37:50 -0400

    Added buttons and started on button event listeners

--- app/src/main/java/io/github/segbk/calculatorapp/MainActivity.java
+++ app/src/main/java/io/github/segbk/calculatorapp/MainActivity.java
@@ -2,12 +2,29 @@ package io.github.segbk.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Button> Buttons = new ArrayList<Button>();
+    int[] IDs =
+            {R.id.btn_one,R.id.btn_two, R.id.btn_three,
+            R.id.btn_four, R.id.btn_five, R.id.btn_six,
+            R.id.btn_seven, R.id.btn_eight, R.id.btn_nine,
+            R.id.btn_zero, R.id.btn_zero, R.id.btn_equals};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int id : IDs){
+            Buttons.add((Button)findViewById(id));
+        }
+        for(Button b : Buttons){
+            b.setOnClickListener();
+        }
     }
 }
---
+++ app/src/main/java/io/github/segbk/calculatorapp/listeners/ButtonOnClickListener.java
@@ -0,0 +1,16 @@
+package io.github.segbk.calculatorapp.listeners;
+
+import android.view.View;
+
+import OnClickListener;
+
+/**
+ * Created by Brennan on 10/2/2016.
+ */
+
+public class ButtonOnClickListener extends View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
